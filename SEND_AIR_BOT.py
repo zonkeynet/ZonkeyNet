1bc789a837add4ea066ba5cc88b5cf2a01bf2978
ZonkeyNet <zonkeynet@gmail.com>
2017-07-07 00:11:23 +0200

    Update SEND_AIR_BOT.py

--- SEND_AIR_BOT.py
+++ SEND_AIR_BOT.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+"""
+AirBot TX - bridge between your preferred IRC to the Airchat/ZonkeyNet radio users
+by (valexxx@autistici.org)
+Send messages from IRC to radio
+"""
+################################################################### 
+# Code from project: ZonkeyNet (Mesh Radio Networks)
+###################################################################
+
 import socket
 import requests
 import time
