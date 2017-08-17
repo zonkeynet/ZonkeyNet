2907c9ca232b5de6505356096b9f9faa5be912d6
ZonkeyNet <zonkeynet@gmail.com>
2017-08-17 02:50:55 +0200

    Update RECEIVE_AIR_BOT.py

--- RECEIVE_AIR_BOT.py
+++ RECEIVE_AIR_BOT.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
-"""
-AirBot RX - bridge between the Airchat/ZonkeyNet radio users & your preferred IRC 
-by valexxx (valexxx@autistici.org)
-Receive messages sent by radio on IRC channels
-"""
 ################################################################### 
-# Code from project: ZonkeyNet (Mesh Radio Networks)
+# Code from project: ZonkeyNet Mesh Radio Network
 ###################################################################
 
 import irc.client
@@ -15,14 +10,28 @@ import time
 from jaraco.stream import buffer
 import collections
 import thread
+import os
+import fcntl
 
+LOCKFILE = "airbot_rx.lock"
 
-channel_name = "#zonkeynet" #chan on IRC
-log_file_path = "/YourFolder/.AirChatLog.json" #Change with your AirChat/Zonkey folder
-IRC_server_address = "127.0.0.1" #Change it with your favorite IRC server
-IRC_nickname = "airBOT_test"
+lfnam = os.path.abspath(LOCKFILE)
+lf = open(lfnam, 'w')
+try:
+    fcntl.lockf(lf, fcntl.LOCK_EX | fcntl.LOCK_NB)
+except BlockingIOError:
+    exit()
 
-NEW_MSGS_ONLY = True #change to False If you want to post all messages trasmitted
+NEW_MSGS_ONLY = True
+# don't set it too low - may result with server ban!
+# (in seconds)
+WAIT_TO_RECONNECT = 30
+
+channel_name = "#ZonkeyNet"
+log_file_path = "/home/pi/ZonkeyNet/.AirChatLog.json" # Your Log file Location
+IRC_server_address = "chat.freenode.net"
+IRC_nickname = "airBOT_RX"
+IRC_passwd = "password" # Change This!
 
 old_msgs = {}
 f2 = open(log_file_path)
@@ -31,12 +40,15 @@ if NEW_MSGS_ONLY == True:
     for key2 in log2:
         old_msgs[key2] = True
 
-
-def endofmotd(con1, event1):
+def endofmotd(con1,event1):
     server1.join(channel_name)
-    thread.start_new_thread(send_all_logged_msgs, ())
-    # send_all_logged_msgs()
+    thread.start_new_thread(send_all_logged_msgs,())
+    #send_all_logged_msgs()
 
+def disco(con1, event1):
+    time.sleep(WAIT_TO_RECONNECT)
+    server1.reconnect()
+    
 def send_all_logged_msgs():
     global old_msgs
     sent = {}
@@ -46,34 +58,36 @@ def send_all_logged_msgs():
         try:
             log1 = json.load(f1)
             log1_sorted = collections.OrderedDict(sorted(log1.items(), key=lambda t: t[1]["timestamp"]))
-            #i = 1
+            #i=1
             for key1 in log1_sorted:
-                # print log1[key1]["content"]
+                #print log1[key1]["content"]
                 if log1_sorted[key1]["txrx"] == "rx":
-                    msg_to_send_noR = log1_sorted[key1]["content"].replace("\r", "")
-                    msg_to_send_noRN = msg_to_send_noR.replace("\n", "")
+                    msg_to_send_noR = log1_sorted[key1]["content"].replace("\r","")
+                    msg_to_send_noRN = msg_to_send_noR.replace("\n","")
                     msg_to_send_noRN = msg_to_send_noRN[:480]
-                    # if i not in sent:
+                    #if i not in sent:
                     if key1 not in sent and key1 not in old_msgs:
-                        server1.privmsg(channel_name, msg_to_send_noRN)
+                        server1.privmsg(channel_name,msg_to_send_noRN)
                         log1.pop(key1)
-                        # print log1[key1]["content"]
-                        # sent[i] = True
-                        sent[key1] = True
+                        #print log1[key1]["content"]
+                        #sent[i] = True
+                        sent[key1]=True
                         time.sleep(10)
-                        # i += 1
+                        #i += 1
         except Exception, e:
             print e
             continue
-
-
+        
+    
+    
 irc.client.ServerConnection.buffer_class = buffer.LenientDecodingLineBuffer
 
-#logging.basicConfig(level = logging.DEBUG)  # comment out for debug
+#logging.basicConfig(level=logging.DEBUG)  # commented out for debug
 irc_client1 = irc.client.Reactor()
 server1 = irc_client1.server()
-server1.connect(IRC_server_address, 6667, IRC_nickname)
+server1.connect(IRC_server_address, 6667, IRC_nickname, IRC_passwd)
 
 irc_client1.add_global_handler("endofmotd", endofmotd)
+irc_client1.add_global_handler("disconnect", disco)
 
 irc_client1.process_forever()
